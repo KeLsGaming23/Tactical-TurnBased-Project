c8389a0205730083cfe481603b0d8f7f453a7ebd
KelsGaming <alcorizamichael@gmail.com>
2025-11-20 04:20:42 +0800

    Unit Animator Parameters
    Setup Parameter Condition Transition in the animator and to unit.cs from walking state to idle state

--- Tactical-TurnBased-Project/Assets/Script/Unit.cs
+++ Tactical-TurnBased-Project/Assets/Script/Unit.cs
@@ -4,6 +4,7 @@ namespace kelsgaming.site
 {
     public class Unit : MonoBehaviour
     {
+        [SerializeField] private Animator unitAnimator;
         private Vector3 targetPosition;
         private void Update()
         {
@@ -13,6 +14,11 @@ namespace kelsgaming.site
                 Vector3 moveDirection = (targetPosition - transform.position).normalized;
                 float moveSpeed = 4f;
                 transform.position += moveDirection *moveSpeed * Time.deltaTime;
+                unitAnimator.SetBool("IsWalking", true);
+            }
+            else
+            {
+                unitAnimator.SetBool("IsWalking", false);
             }
             if (Input.GetMouseButtonDown(0))
             {
