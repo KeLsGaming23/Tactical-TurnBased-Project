b2efdffd59cb3491a4cfa6874b7794d524d00e5d
KelsGaming <alcorizamichael@gmail.com>
2025-11-19 17:57:25 +0800

    Mouse Raycast
    Test out Physics.Raycast if it hit any collider in the scene

---
+++ Tactical-TurnBased-Project/Assets/Script/MouseWorld.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace kelsgaming.site
+{
+    public class MouseWorld : MonoBehaviour
+    {
+        private void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(Physics.Raycast(ray));
+        }
+    }
+}
